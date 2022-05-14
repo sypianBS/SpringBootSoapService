3c866ab20c5cd527713374d3d32ef8f9c9c3e58c
sypianBS <sypianbs@gmail.com>
2022-05-14 12:51:54 +0200

    generate java code from xsd scheme using xjc

---
+++ src/main/xsd/src/com/suapwebs/xjc/ObjectFactory.java
@@ -0,0 +1,63 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2022.05.14 um 12:46:41 PM CEST 
+//
+
+
+package com.suapwebs.xjc;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.suapwebs.xjc package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.suapwebs.xjc
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link GetResponse }
+     * 
+     */
+    public GetResponse createGetResponse() {
+        return new GetResponse();
+    }
+
+    /**
+     * Create an instance of {@link Student }
+     * 
+     */
+    public Student createStudent() {
+        return new Student();
+    }
+
+    /**
+     * Create an instance of {@link GetStudent }
+     * 
+     */
+    public GetStudent createGetStudent() {
+        return new GetStudent();
+    }
+
+}
